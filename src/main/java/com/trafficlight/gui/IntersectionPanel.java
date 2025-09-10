2563e037b8120ce067f37feab6b46758b9464d9b
asyasser <assouyasser1@gmail.com>
2025-09-10 02:28:42 +0100

    Update IntersectionPanel.java

--- src/main/java/com/trafficlight/gui/IntersectionPanel.java
+++ src/main/java/com/trafficlight/gui/IntersectionPanel.java
@@ -25,13 +25,13 @@ public class IntersectionPanel extends JPanel {
         setPreferredSize(new Dimension(600, 400));
         setBackground(Color.DARK_GRAY);
 
-        // Initialize some default intersections
+        
         addIntersection("TL1", 3, 3);
         addIntersection("TL2", 6, 3);
         addIntersection("TL3", 3, 6);
         addIntersection("TL4", 6, 6);
 
-        // Start a timer to repaint the panel
+        
         Timer timer = new Timer(500, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -78,7 +78,7 @@ public class IntersectionPanel extends JPanel {
         g2d.setColor(Color.GRAY);
         g2d.setStroke(new BasicStroke(1));
 
-        // Draw grid lines
+        
         for (int i = 0; i <= getWidth(); i += GRID_SIZE) {
             g2d.drawLine(i, 0, i, getHeight());
         }
@@ -91,13 +91,13 @@ public class IntersectionPanel extends JPanel {
         g2d.setColor(Color.LIGHT_GRAY);
         g2d.setStroke(new BasicStroke(3));
 
-        // Draw horizontal roads
+        
         for (Point pos : intersectionPositions.values()) {
             int y = pos.y * GRID_SIZE + GRID_SIZE/2;
             g2d.drawLine(0, y, getWidth(), y);
         }
 
-        // Draw vertical roads
+        
         for (Point pos : intersectionPositions.values()) {
             int x = pos.x * GRID_SIZE + GRID_SIZE/2;
             g2d.drawLine(x, 0, x, getHeight());
@@ -113,16 +113,16 @@ public class IntersectionPanel extends JPanel {
             int x = pos.x * GRID_SIZE + GRID_SIZE/2 - INTERSECTION_SIZE/2;
             int y = pos.y * GRID_SIZE + GRID_SIZE/2 - INTERSECTION_SIZE/2;
 
-            // Draw intersection background
+            
             g2d.setColor(Color.BLACK);
             g2d.fillRect(x, y, INTERSECTION_SIZE, INTERSECTION_SIZE);
 
-            // Draw traffic light color
+            
             Color lightColor = getTrafficLightColor(state);
             g2d.setColor(lightColor);
             g2d.fillOval(x + 2, y + 2, INTERSECTION_SIZE - 4, INTERSECTION_SIZE - 4);
 
-            // Draw intersection ID
+            
             g2d.setColor(Color.WHITE);
             g2d.setFont(new Font("Arial", Font.BOLD, 10));
             FontMetrics fm = g2d.getFontMetrics();
@@ -142,10 +142,10 @@ public class IntersectionPanel extends JPanel {
             int x = pos.x * GRID_SIZE + GRID_SIZE/2 - VEHICLE_SIZE/2;
             int y = pos.y * GRID_SIZE + GRID_SIZE/2 - VEHICLE_SIZE/2;
 
-            // Draw vehicle as a small circle
+            
             g2d.fillOval(x, y, VEHICLE_SIZE, VEHICLE_SIZE);
 
-            // Draw vehicle ID
+            
             g2d.setColor(Color.CYAN);
             g2d.setFont(new Font("Arial", Font.PLAIN, 8));
             FontMetrics fm = g2d.getFontMetrics();
@@ -168,19 +168,19 @@ public class IntersectionPanel extends JPanel {
         g2d.setFont(new Font("Arial", Font.BOLD, 12));
         g2d.drawString("Legend:", legendX, legendY);
 
-        // Red light
+        
         g2d.setColor(Color.RED);
         g2d.fillOval(legendX, legendY + 5, 10, 10);
         g2d.setColor(Color.WHITE);
         g2d.drawString("Red Light", legendX + 15, legendY + 15);
 
-        // Yellow light
+        
         g2d.setColor(Color.YELLOW);
         g2d.fillOval(legendX, legendY + 20, 10, 10);
         g2d.setColor(Color.WHITE);
         g2d.drawString("Yellow Light", legendX + 15, legendY + 30);
 
-        // Green light
+        
         g2d.setColor(Color.GREEN);
         g2d.fillOval(legendX, legendY + 35, 10, 10);
         g2d.setColor(Color.WHITE);
